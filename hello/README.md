e79a2fc3f5e25a111f239f2bd61ab7458373bb2c
ROS DUL <rosdul@ROSs-MacBook-Pro.local>
2025-08-25 22:00:54 +0900

    hello

---
+++ hello/README.md
@@ -0,0 +1,7 @@
+### Increase button
+basic of using count number.
+### Input value
+basic learning input value and show value
+
+![alt](./src/lib/assets/input.jpg)
+
---
+++ hello/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    let num = 0
+    function increase(){
+        num += 1
+    }
+    // Input
+    let inputText = ""
+</script>
+
+<div>
+    <h1>{num}</h1>
+    <button on:click={increase}>increase</button>
+</div>
+<hr/>
+
+<div>
+    <p>hello {inputText}!</p>
+    <input type="text" bind:value={inputText} >
+
+</div>
+
+
+    
+
